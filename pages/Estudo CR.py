6f1f1f9f1b09ea0137caebe321838059c9152125
vspadilha <contato.vitors@gmail.com>
2024-02-10 22:59:42 -0300

    Update Estudo CR.py
    Fix Locale pós reset

--- pages/Estudo CR.py
+++ pages/Estudo CR.py
@@ -11,8 +11,8 @@ import json
 
 
 #Configurações
-#locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
-locale.setlocale(locale.LC_ALL, '')
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+#locale.setlocale(locale.LC_ALL, '')
 
 my_bar = st.progress(0, text='')
 
